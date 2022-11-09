d0ae4f26f43d6c89f4fdc11fb23ee4f2069fe8b9
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-09 20:01:13 +0600

    added review form

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaUserCircle, FaStar, } from "react-icons/fa";
+import ReviewForm from '../ReviewForm/ReviewForm';
 
 const Details = () => {
     const serviceDetails = useLoaderData();
@@ -8,19 +9,24 @@ const Details = () => {
 
     return (
 
-        <section>
-            <div className="">
-                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
-                    <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-900">{title}</h1>
-                    <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-900">{description}</p>
-                    <div className="flex flex-wrap justify-center">
-                        <h2 className='px-8 py-3 m-2 text-lg font-semibold rounded bg-gray-800 text-gray-50'>Price : ${price}</h2>
+        <div>
+            <section>
+                <div className="">
+                    <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
+                        <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-900">{title}</h1>
+                        <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-900">{description}</p>
+                        <div className="flex flex-wrap justify-center">
+                            <h2 className='px-8 py-3 m-2 text-lg font-semibold rounded bg-gray-800 text-gray-50'>Price : ${price}</h2>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <img src={img} alt="" className="w-3/5 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 bg-gray-500" />
-        </section>
+                <img src={img} alt="" className="w-3/5 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 bg-gray-500" />
+            </section>
+            <section className='my-10'>
+                <ReviewForm></ReviewForm>
+            </section>
+        </div>
 
     );
 };
