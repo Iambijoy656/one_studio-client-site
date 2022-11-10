9c175e09656412965263824d6ca8935e3a7ffccf
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-10 23:05:24 +0600

    added photo view

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 
 const ServiceCard = ({ service }) => {
     const { _id, img, title, price, description } = service;
     return (
         <div className="max-w-xs rounded-md shadow-md bg-white text-gray-900">
-            <img src={img} alt="" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500" />
+
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <img src={img} alt="" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500" />
+                </PhotoView>
+            </PhotoProvider>
 
             <div className="flex flex-col justify-between p-6 space-y-8">
                 <div className="space-y-2">
--- src/Pages/ServicePage/ServicePage.js
+++ src/Pages/ServicePage/ServicePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PhotoProvider } from 'react-photo-view';
 import { useLoaderData } from 'react-router-dom';
 import ServiceCard from '../Home/Services/ServiceCard';
 
@@ -13,6 +14,7 @@ const ServicePage = () => {
             <h2 className='text-6xl text-gray-700 text-center font-semibold mt-5'>Focus Service</h2>
             <p className='text-center text text-gray-600  text-xl w-9/12 mx-auto font-semibold my-5'>To all my clients, we offer a wide range of quality services in the field of wedding photo and video shooting. When you need to make the right impression, you need a professional with the vision to make it a reality. I professional who can provide all these services and more.</p>
 
+
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-9/12 mx-auto mt-10'>
                 {
                     services.map(service => <ServiceCard
@@ -23,6 +25,7 @@ const ServicePage = () => {
                     ></ServiceCard>)
                 }
             </div>
+
         </div>
     );
 };
